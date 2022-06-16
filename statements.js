858746df97799a2a5b41f026f2ff15d10e3763a9
Krishan Gharu <kslg257@gmail.com>
2022-06-16 21:27:45 +0000

    updates

--- statements.js
+++ statements.js
@@ -36,3 +36,55 @@ $("a:last");
 
 // Select all header elements (h1, h2, h3, h4, h5, h6)
 $(":header");
+
+// Get all img elements on the page
+$("img");
+
+// Get all elements with a class of `card_image`
+$(".card_image");
+
+// Get the footer by its ID
+$('#my_footer');
+
+// All paragrapgh elements in the `footer`
+$("footer p");
+
+// Get all header elements (h1-h6)
+$(":header");
+
+// Get first element on the page
+$(":first");
+
+// Get the last `div` on the page
+$("div:last")
+
+// Get the last `img` on the page
+$("img:last");
+
+// Get all elements that have an attribute of `href`
+$("[href]")
+
+// change css color of all list elements
+$("li").css("color","red");
+
+// underlines all h2 elements
+$("h2").css("text-decoration","underline");
+
+// add a 1px border to all lists
+$("ul").css("border","solid 1px #ccc");
+
+// change html text for the footer class id
+$("#my_footer").html("<h1>I can change the text in the DOM</h1>");
+
+// change text for the footer class id, keep original styles
+$("#my_footer").text("I can change the text in the DOM");
+
+// add a new element next to existing.
+$("#my_footer").append("<p>This is a new element</p>");
+
+// add a new element at the end
+$("#copyright").append("<span>&copy; Copyright 2022</span>");
+
+// change all p elements in a class
+$(".card p").text("All changed");
+
---
+++ table/js/my_script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $("th").css("color","red");
+    $("h2").css("text-decoration", "underline"); // underline all h2 elements
+    
+})
