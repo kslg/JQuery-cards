fcb4c170f9a0ca95872d466ee75c57e261ee4101
Krishan Gharu <kslg257@gmail.com>
2022-06-17 16:16:32 +0000

    updates

--- script.js
+++ script.js
@@ -25,9 +25,9 @@ $(document).ready(function() {
     $("#button_effects1").click(function(){
         $('#par1').slideToggle('1000');
     });
-    $("#button_effects2").click(function(){
-        $('#par2').slideToggle('1000');
-    });
+    // $("#button_effects2").click(function(){
+    //     $('#par2').slideToggle('1000');
+    // });
     $("#button_effects3").click(function(){
         $('#par3').slideToggle('1000');
     });
